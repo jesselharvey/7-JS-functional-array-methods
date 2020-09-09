7a98347eef155b68db15b16f40e0ef9e1a7279d7
Jesse Harvey <jesselharvey@gmail.com>
2020-09-08 21:26:32 -0700

    answered questions 1-3

--- main.js
+++ main.js
@@ -1,30 +1,53 @@
 // const prices = items
 //   .map(function (item) {
 //     console.log(prices)
-
 //     const total = items.reduce(function (a, b) {
 //       return a + b.price
 //     }, 0)
-//     return  total / price.length
-
-//     return prices
-//   }).join('')
+//     return total / item.length
+//   })
+//   .join("")
 
 // document.querySelector("#answer1").innerHTML = prices
 
+// const filtered = items.filter(function (item) {
+// return item.price
+// })
+// const filtered2 = filtered
+//   .map(function (item) {
+//     return item.price
+//   })
+//   .join("")
+
+// document.querySelector("#answer2").innerHTML = filtered2
+
+const sum = items.reduce(function (x, y) {
+  return x + y.price
+}, 0)
+const average = (sum / items.length).toFixed(2)
+document.querySelector(
+  "#answer1"
+).innerHTML = `Average price of all items is $${average}`
+
+//------
+
 const filtered = items.filter(function (item) {
-  if (item.price > 14) {
-    return item.price
-  } else if (item.price < 18) {
-    return item.price
-  } else {
-    return false
-  }
-}) 
-const filtered2 = filtered
+  return item.price >= 14 && item.price <= 18
+})
+const finalFiltered = filtered
   .map(function (item) {
-    return item.price
+    return `<li>${item.title}</li>`
   })
   .join("")
+document.querySelector("#answer2").innerHTML = finalFiltered
 
-document.querySelector("#answer2").innerHTML = filtered2
+//-----
+
+const gBPfilter = items.filter(function (item) {
+  console.log(item)
+  return item.currency_code === `GBP`
+})
+const gBPfiltered = gBPfilter.map(function (item) {
+  return `<li>Item: ${item.title}</li><li>Price: ${item.price}</li><br>`
+})
+document.querySelector("#answer3").innerHTML = gBPfiltered
